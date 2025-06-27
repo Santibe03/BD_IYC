7292663b2ecf6ae831fcd10aedf2df01a1ca98f0
Areho <alejocon6907@gmail.com>
2025-06-26 23:46:08 -0500

    commit 4

--- uso.sql
+++ uso.sql
@@ -55,15 +55,3 @@ CALL insertarPedido(2, 1); -- mesa 2, estado 1
 -- Luego inserta un producto a ese pedido (sabiendo el ID):
 CALL insertarProductoPedido(8, 3, 2, 15000); -- pedido 1, producto 3, 2 unidades, $150
 
-
-
-
-
-
-
-
-
-
-
-
-
